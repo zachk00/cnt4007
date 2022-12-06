b8286812ea77c583ea8bfd8b58cac919ffed69a3
Zachary Kerr <zacharykerr@ufl.edu>
2022-12-06 00:49:12 -0500

    Add files via upload

---
+++ src/torent/messages/Message.java
@@ -0,0 +1,149 @@
+package torent.messages;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class Message {
+	
+	final String handshakeMSG = "P2PFILESHARINGPROJ";
+	
+	public Message() {}
+	
+	public byte[] handshake(int peerID) {
+		byte[] header = handshakeMSG.getBytes();
+		byte[] zeros = new byte[10];
+		
+		ByteBuffer temp = ByteBuffer.allocate(4);
+		temp.putInt(peerID);
+		byte[] id = temp.array();
+		
+		byte[] handshakeTemp = new byte[header.length + zeros.length + id.length];
+		
+		ByteBuffer handshake = ByteBuffer.wrap(handshakeTemp);
+		handshake.put(header);
+		handshake.put(zeros);
+		handshake.put(id);
+		
+		ByteBuffer buffer =  ByteBuffer.wrap(handshake.array());
+        byte[] h = new byte[18];
+        buffer.get(h,0,18);
+        String handShakeString = new String(h, StandardCharsets.UTF_8);
+		System.out.println("handshake maker " + handShakeString);
+		
+		
+		return handshake.array();
+		
+	}
+	
+	public byte[] createMessage(int type, messageParams params) throws IOException, NullParamException {
+		
+		ByteArrayOutputStream message = new ByteArrayOutputStream();
+		
+		
+		byte[] payload;
+		ByteBuffer length;
+		
+		int messageLength = 0;
+		
+		byte[] messageType = new byte[1];
+		messageType[0] = (byte) type;
+		
+		
+		
+		switch(type) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+				// choke, unchoke, interested, not interested messages
+				messageLength = messageType.length;
+				
+				length = ByteBuffer.allocate(4);
+				length.putInt(messageLength);
+				
+				message.write(length.array());
+				message.write(messageType);
+				break;
+			case 4:
+			case 6:
+				// have message(4) request message(6)
+				messageLength = messageType.length;
+				
+				payload = (byte[]) params.getParam("pieceIndex");
+				
+				messageLength = messageLength + payload.length;
+				
+				length = ByteBuffer.allocate(4);
+				length.putInt(messageLength);
+				
+				message.write(length.array());
+				message.write(messageType);
+				message.write(payload);
+				break;
+			case 5:
+				//bitfield message
+				messageLength = messageType.length;
+				
+				payload = (byte[]) params.getParam("bitfield");
+				
+				messageLength = messageLength + payload.length;
+				
+				length = ByteBuffer.allocate(4);
+				length.putInt(messageLength);
+				
+				message.write(length.array());
+				message.write(messageType);
+				message.write(payload);
+			case 7:
+				//piece message
+				messageLength = messageType.length;
+				ByteBuffer temp;
+				
+				byte[] pieceField = (byte[]) params.getParam("bitfield");
+				int pieceIndex = (int) params.getParam("pieceIndex");
+				
+				temp = ByteBuffer.allocate(4 + pieceField.length);
+				temp.putInt(pieceIndex);
+				temp.put(pieceField);
+				
+				payload = temp.array();
+						
+				messageLength = messageLength + payload.length;
+				
+				length = ByteBuffer.allocate(4);
+				length.putInt(messageLength);
+				
+				message.write(length.array());
+				message.write(messageType);
+				message.write(payload);
+			default:
+				System.out.println("INVALID MESSAGE TYPE");
+				throw new IllegalArgumentException("Message of type: " + String.valueOf(type) + " does not exist");
+				
+		}
+		
+		
+		return message.toByteArray();
+	}
+	
+	public int getType(byte[] msg) {
+		return msg[4];
+	}
+	
+	public byte[] getPayload(byte[] msg) {
+		
+		// copy from message starting at 5th byte (length + type fields)
+		// insert into payload array starting at 0th byte
+		// copy message length - 5 bytes into payload (the five parts are the len & type)
+		
+		byte[] payload = new byte[msg.length - 5];
+		System.arraycopy(msg, 5, payload, 0, msg.length-5);
+		
+		return payload;
+		
+	}
+
+}
--- src/torent/messages/handler.java
+++ src/torent/messages/handler.java
@@ -6,7 +6,9 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
+import torent.logger.pLogger;
 import torent.peer.peer;
 
 public class handler implements Runnable{
@@ -15,13 +17,19 @@ public class handler implements Runnable{
 	private ObjectInputStream in;
 	private peer currPeer;
 	private Socket socket;
-	private message creator = new message();
+	private volatile boolean running = true;
+	private Message creator;
+	private pLogger log;
+	private int otherPeerID;
+	
 	
 	public handler(ObjectOutputStream out, ObjectInputStream in, peer currPeer, Socket sock) {
 		this.currPeer = currPeer;
 		this.out = out;
 		this.in = in;
 		this.socket = sock;
+		this.creator = new Message();
+		this.log = new pLogger(String.valueOf(this.currPeer.getPeerID()));
 	}
 	
 	
@@ -32,28 +40,124 @@ public class handler implements Runnable{
 		
 		
 		try{
-		while(true)
-		{
-			System.out.println("handler while");
-			out = new ObjectOutputStream(this.socket.getOutputStream());
-			out.flush();
-			System.out.println("outs");
-			in = new ObjectInputStream(this.socket.getInputStream());
-			System.out.println("in sock");
-			//receive the message sent from the client
-			String message = (String)in.readObject();
-			System.out.println("read sock");
-			//show the message to the user
-			System.out.println("READ : " + message);
+			//Initial handshake
+			byte[] handshake = creator.handshake(this.currPeer.getPeerID());
+			this.currPeer.transmit(out, handshake);
 			
 		}
-		}
 		catch(Exception e) {
-			System.out.println("AH");
+			System.out.println("Inital Handshake Failed: " + this.currPeer.toString());
 			e.printStackTrace();
 		}
 		
+		while(running)
+		{
+			// if everyone has the file, we start terminating threads
+			// when it reaches the end of this run method the handler thread will exit
+			if (this.currPeer.hasFile() && this.currPeer.neighborsHaveFile()) {
+				stop();
+			}
+			try {
+				byte [] message = (byte[])in.readObject();
+				
+				int type = creator.getType(message);
+				
+				switch (type) {
+					case 0:
+						// choked by neighbor
+						
+						this.log.choked(String.valueOf(this.otherPeerID));
+						break;
+					case 1:
+						// unchoke from neighbor
+						
+						
+						this.log.unchoked(String.valueOf(this.otherPeerID));
+						
+						if(!this.currPeer.hasFile()) {
+							
+							// if peer receives unchoke msg
+							// peer should respond with request message
+							
+							
+							
+							
+						}
+						
+						break;
+					case 2:
+						// interested
+						
+						this.currPeer.addInterestedPeer(this.otherPeerID);
+						this.log.recInterested(String.valueOf(this.otherPeerID));
+						
+						break;
+					case 3:
+						// not interested
+						this.currPeer.removeInterestedPeer(this.otherPeerID);
+						this.log.recNotInterested(String.valueOf(this.otherPeerID));
+						break;
+					case 4:
+						// have
+						break;
+					case 5:
+						// bitfield
+						break;
+					case 6:
+						// request
+						break;
+					case 7:
+						// piece
+						
+						break;
+					default:
+						// handshake
+						ByteBuffer handshake = ByteBuffer.wrap(message);
+						byte[] temp = new byte[18];
+						
+						handshake.get(temp, 0, 18);
+						
+						String handShakeString = new String(temp, StandardCharsets.UTF_8);
+						
+						if(handShakeString.equalsIgnoreCase(this.creator.handshakeMSG)) {
+							
+							
+							// respond with bitfield
+							// create proper param object 
+							// use message class to create byte array to send
+	
+							messageParams params = new messageParams();
+							params.setBitfield(this.currPeer.getBitfield());
+							byte[] response = creator.createMessage(5, params);
+							this.currPeer.transmit(out, response);
+							
+							// extract the peerID of the other peer
+							// save in to this peer's contact map
+							byte[] otherPeerID = new byte[4];
+							System.arraycopy(message, 28, otherPeerID, 0, 4);
+							int id = ByteBuffer.wrap(otherPeerID).getInt();
+							
+							this.currPeer.getContact().put(id, out);
+							
+							log.recTCP(String.valueOf(id));
+							
+						}
+						
+				}
+				
+				
+			}
+			catch (Exception e) {
+				System.out.println("Error processing message in handler");
+				e.printStackTrace();
+			}
 		
+		}
+		
+	}
+	
+	public void stop() {
+		this.running = false;
 	}
 	
 }
