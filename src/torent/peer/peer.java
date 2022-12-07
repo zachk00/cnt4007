bc4d85fc7d25dbb0f722e374322355e846302823
Zachary Kerr <zacharykerr@ufl.edu>
2022-12-06 20:00:28 -0500

    Add files via upload

--- src/torent/peer/peer.java
+++ src/torent/peer/peer.java
@@ -14,6 +14,8 @@ import torent.client;
 import torent.server;
 import torent.logger.pLogger;
 import torent.messages.Message;
+import torent.messages.NullParamException;
+import torent.messages.messageParams;
 
 public class peer {
 	//props from PeerInfo.cfg
@@ -25,8 +27,10 @@ public class peer {
 	// peer attributes
 	private ObjectOutputStream out;
 	private BitSet bitfield;
-	List<Integer> interestedPeers;
-	
+	private List<Integer> interestedPeers;
+	private List<Integer> chockedPeers;
+	private List<Integer> unchockedPeers;
+	private int optimisticallyUnchockedPeer;
 	// tools
 	private Message creator;
 	private pLogger log;
@@ -52,16 +56,91 @@ public class peer {
 	
 	HashMap<Integer, ObjectOutputStream> contact;
 	Map<Integer, peer> peersInfo;
+	HashMap<Integer, BitSet> peersInterestingPieces;
 	
+	
+	
+	
+	public HashMap<Integer, BitSet> getPeersInterestingPieces() {
+		return peersInterestingPieces;
+	}
+
+
+
+
+	public void setPeersInterestingPieces(int fromPeerID, BitSet fromBitSet) throws IOException, NullParamException {
+		
+		// need to handle whether we already do/dont have this peer's bitset
+		// whether theres anything interesting in that peer'sbitset
+		
+		messageParams params = new messageParams();
+		
+		
+		
+		if (this.peersInterestingPieces.containsKey(fromPeerID)) {
+			BitSet latest = this.peersInterestingPieces.get(fromPeerID);
+			if (latest.isEmpty()) {
+				
+				 // the peer has nothing for us, so we dont need to store their bitfield
+				// we also need to send them a not interested msg
+				
+				this.peersInterestingPieces.remove(fromPeerID);
+				
+				try {
+					byte [] msg = creator.createMessage(3 , params);
+					this.transmit(this.contact.get(fromPeerID), msg);
+				}
+				catch(Exception e) {
+					System.out.println("Error transmiting not interested msg after receiving bitfield");
+					e.printStackTrace();
+				}
+			}
+			else {
+				if (!latest.isEmpty())
+				this.peersInterestingPieces.replace(fromPeerID, latest);
+				
+				try {
+					byte [] msg = creator.createMessage(2 , params);
+					this.transmit(this.contact.get(fromPeerID), msg);
+				}
+				catch(Exception e) {
+					System.out.println("Error transmiting interested msg after receiving bitfield");
+					e.printStackTrace();
+				}
+			}
+		}
+		else {
+			this.peersInterestingPieces.put(fromPeerID, fromBitSet);
+			
+			try {
+				byte [] msg = creator.createMessage(2 , params);
+				this.transmit(this.contact.get(fromPeerID), msg);
+			}
+			catch(Exception e) {
+				System.out.println("Error transmiting interested msg after receiving bitfield");
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+
+
+
+
 	public peer(int peerID, int port, String hostname, int hasFile) {
 		this.peerID = peerID;
 		this.port = port;
 		this.hostname = hostname;
 		this.hasFile = hasFile;
-		this.interestedPeers = new ArrayList<>();
+		this.interestedPeers = new ArrayList<Integer>();
 		this.creator = new Message();
 		this.log  = new pLogger(String.valueOf(this.peerID));
 		this.contact = new HashMap<Integer, ObjectOutputStream>();
+		this.peersInterestingPieces = new HashMap<Integer, BitSet>();
+		
+		this.unchockedPeers = new ArrayList<Integer>();
+		this.chockedPeers = new ArrayList<Integer>(); 
 		
 	}
 	
@@ -227,22 +306,82 @@ public class peer {
 	}
 	
 	public void updateBitfield(int requestIndex) {
-		
+		/// toDo
 	}
 	
+	public BitSet findInterestingPieces(BitSet fromBitfield) {
+		
+		// TODO
+		// 
+		// compare this peer's bitfield with given bitset
+		
+		// return bitset that has the interesting pieces demarkated as true
+		
+		// so ex. this.peer bitset =  1 0 1 1 1
+	    // the from bitfield has 1 1 0 0 0
+		// our bitfield needs the 2nd piece and the from peer has it so we return
+		// 0 1 0 0 0
+		
+		// this return statement is just so eclipse doesnt yell at me, you'll need to change this
+		return new BitSet();
+	}
+	
+	public int getRequestIndex(BitSet interestingPieces) {
+		int index = 0;
+		
+		//TODO
+		//given bitset of interesting pieces
+		// randomly select one and return the index
+		
+		
+		return index;
+	}
+	
+	// feel free to change method signature as needed/ add helpers methods
+	//will call this from peerProcess for each peer to start the process
+	// it calls prefferd peers every X interval based on config
+	//probably need a thread
+	public void runPreferredPeers() {
+		// TODO
+	}
+	
+	// feel free to change method signature as needed/ add helpers methods
+	// selected new preffered peers
+	public void preferredPeers() {
+		// TODO
+	}
+	
+	
+	// feel free to change method signature as needed/ add helpers methods
+	//will call this from peerProcess for each peer to start the process
+	// it calls poptimisticUnchoke every X interval based on config
+	//probably need a thread
+	public void runOptimistic() {
+		// TODO
+	}
+	
+	// feel free to change method signature as needed/ add helpers methods
+	// select optimisticUnchoke neighbors
+	public void optimisticUnchoke() {
+		// TODO
+	}
+	
+	
+	
 	// reference note: port & host should be inside of the msg array
-		public void transmit(ObjectOutputStream stream, byte[] msg) throws IOException {
-			try {
-				stream.writeObject(msg);
-				stream.flush();
-			}
-			catch(Exception e) {
-				System.out.println("Issue sending msg");
-				e.printStackTrace();
-			}
+	public void transmit(ObjectOutputStream stream, byte[] msg) throws IOException {
+		try {
+			stream.writeObject(msg);
+			stream.flush();
 			System.out.println("transmitted");
-			
 		}
+		catch(Exception e) {
+			System.out.println("Issue sending msg");
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 	public boolean neighborsHaveFile() {
@@ -254,6 +393,17 @@ public class peer {
 		
 		return true;
 	}
+	
+	public void writeFile() {
+		
+		//TODO
+		// all the data for the file is stored in the byte[][] file
+		// save it to a file in the appropiate folder
+		// you can assume the folder already exists
+		// we will have the folders created before we run anything
+	}
+	
+	
 	
 	@Override
 	public String toString() {
